a8c91594cb535819b46359095fa285cf319fce07
MYCURE <mycure@EJs-MacBook-Pro.local>
2025-03-04 12:11:51 +0800

    Almost done

---
+++ main.js
@@ -0,0 +1,109 @@
+import { readFile } from 'fs';
+import { writeFile } from 'fs/promises';
+
+//Read Stream file
+async function ingest()
+{
+    let ingest = await readFile('users.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        else 
+        {
+            //Array of objects
+            let json = JSON.parse(data);
+
+            //Remove duplicates
+            //using hashmap, key = id, val = array containing indexes
+            let hm = new Map();
+
+            for (let i = 0; i <= json.length - 1; i++)
+            {
+                //If no key
+                //Add idx to json obj
+                let obj = json[i];
+                obj['idx'] = i;
+                if (hm.has(obj.id) === false)
+                {
+                    let arr = [];
+                    arr.push({id: obj.id, idx: i});
+                    hm.set(obj.id, arr);
+                }
+
+                //If key exists
+                else 
+                {
+                    let arr = hm.get(obj.id);
+                    arr.push({id: obj.id, idx: i});
+                    hm.set(obj.id, arr);
+                }
+            }
+
+            //Cleaned hashmap
+            let cleaned = new Map();
+            hm.forEach((val, key) => {
+                let arr = val;
+                //Duplicates are present
+                if (val.length > 1)
+                {
+                    for (let j = arr.length - 1; j > 0; j--)
+                    {
+                       arr.splice(j, 1); 
+                    }
+                }
+                
+                cleaned.set(key, arr);
+            })
+
+            //Make new JSON object
+            let newjson = [];
+            let mapkeys = hm.keys();
+            for (let k of mapkeys)
+            {
+                let userobj = {};
+                let jsonobj = json.find((o) => o['id'] === k);
+                userobj['id'] = JSON.stringify(jsonobj['id'], null, 2);
+                userobj['name'] = JSON.stringify(jsonobj['name'], null, 2);
+                userobj['dateOfBirth'] = JSON.stringify(jsonobj['dateOfBirth'], null, 2);
+                userobj['mobileNo'] = JSON.stringify(jsonobj['mobileNo'], null, 2);
+                userobj['picURL'] = JSON.stringify(jsonobj['picURL'], null, 2);
+
+                //For the Friends
+                // console.log(jsonobj['friends'])
+                let friendArr = [];
+                for (let friendID of jsonobj['friends'])
+                {
+                    let friendObj = json.find((o) => o['id'] === (friendID));
+
+                    //Delete idx property
+                    delete friendObj['idx'];
+
+                    let jsonified = JSON.stringify(friendObj);
+
+                    friendArr.push(jsonified)
+                    
+                }
+                
+                userobj['friends'] = friendArr;
+
+                //append to end of array 
+                newjson.push(userobj);
+
+            }
+
+            // console.log(newjson)
+            //Making JSON file
+            const jsonData = JSON.stringify(newjson, null, 2); //2 Indentation
+            writeFile('cleaned.json', jsonData, 'utf-8', (err, data) => {
+                if (err) console.error('Data Preprocessing has an error in the system');
+                else console.log('Successfully created the cleaned JSON data!');
+            })
+
+
+
+            
+        }
+})
+
+    return await ingest
+} 
+
+await ingest();
